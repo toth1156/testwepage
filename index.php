242c5655faac14fdcb5a2364cd99e39d17d11941
toth1156 <50401013+toth1156@users.noreply.github.com>
2020-09-19 16:33:07 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>ASD</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <style>
+        body
+        {
+            background-image: url("https://wallpaperaccess.com/full/25557.jpg");
+            background: no-repeat center center cover fixed;
+            -webkit-background-size:  cover;
+            -moz-background-size:no-repeat cover;
+            -o-background-size:  cover;
+            background-size:no-repeat cover;
+            }
+            /* html { 
+            background: url("images/bg.jpg") no-repeat center center fixed; 
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+            } */
+            .navbar {
+  background: none;
+}
+
+    </style>
+    
+    </head>
+    <body class="text-white">
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <nav class="navbar navbar-expand-lg border transparent">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </div>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+            </div>
+          </nav>
+
+
+  <!--       <div class="container border mw-100 mh-100 d-block bg-dark mt-5">
+            <div class="row border mh-100">
+                <div class="col border">
+<pre class="text-center">
+asdasdasggsjkgjfkdg
+gfdlhnd lndflhn ldfnhldfn nfdh
+dhn dflhndéfnh kdfnh 
+dh ndfkh nfdlhn dflkhnndfhnldnf hdf
+gnfgdnlng dfg ,
+asdasdasggsjkgjfkdg
+gfdlhnd lndflhn ldfnhldfn nfdh
+dhn dflhndéfnh kdfnh 
+dh ndfkh nfdlhn dflkhnndfhnldnf hdf
+gnfgdnlng dfg 
+asdasdasggsjkgjfkdg
+gfdlhnd lndflhn ldfnhldfn nfdh
+dhn dflhndéfnh kdfnh 
+dh ndfkh nfdlhn dflkhnndfhnldnf hdf
+gnfgdnlng dfg 
+asdasdasggsjkgjfkdg
+gfdlhnd lndflhn ldfnhldfn nfdh
+dhn dflhndéfnh kdfnh 
+dh ndfkh nfdlhn dflkhnndfhnldnf hdf
+gnfgdnlng dfg 
+asdasdasggsjkgjfkdg
+gfdlhnd lndflhn ldfnhldfn nfdh
+dhn dflhndéfnh kdfnh 
+dh ndfkh nfdlhn dflkhnndfhnldnf hdf
+gnfgdnlng dfg 
+asdasdasggsjkgjfkdg
+gfdlhnd lndflhn ldfnhldfn nfdh
+dhn dflhndéfnh kdfnh 
+dh ndfkh nfdlhn dflkhnndfhnldnf hdf
+gnfgdnlng dfg 
+</pre>
+                </div>
+                <div class="col border" style="height: 80%;">
+<pre class="text-center">
+asdasdasggsjkgjfkdg
+gfdlhnd lndflhn ldfnhldfn nfdh
+dhn dflhndéfnh kdfnh 
+dh ndfkh nfdlhn dflkhnndfhnldnf hdf
+gnfgdnlng dfg ,
+asdasdasggsjkgjfkdg
+gfdlhnd lndflhn ldfnhldfn nfdh
+dhn dflhndéfnh kdfnh 
+dh ndfkh nfdlhn dflkhnndfhnldnf hdf
+gnfgdnlng dfg 
+asdasdasggsjkgjfkdg
+gfdlhnd lndflhn ldfnhldfn nfdh
+dhn dflhndéfnh kdfnh 
+dh ndfkh nfdlhn dflkhnndfhnldnf hdf
+gnfgdnlng dfg 
+asdasdasggsjkgjfkdg
+gfdlhnd lndflhn ldfnhldfn nfdh
+dhn dflhndéfnh kdfnh 
+dh ndfkh nfdlhn dflkhnndfhnldnf hdf
+gnfgdnlng dfg 
+asdasdasggsjkgjfkdg
+gfdlhnd lndflhn ldfnhldfn nfdh
+dhn dflhndéfnh kdfnh 
+dh ndfkh nfdlhn dflkhnndfhnldnf hdf
+gnfgdnlng dfg 
+asdasdasggsjkgjfkdg
+gfdlhnd lndflhn ldfnhldfn nfdh
+dhn dflhndéfnh kdfnh 
+dh ndfkh nfdlhn dflkhnndfhnldnf hdf
+gnfgdnlng dfg 
+</pre>
+                </div>
+                <div class="col border" style="height:20%">
+<pre class="text-center">
+asdasdasggsjkgjfkdg
+gfdlhnd lndflhn ldfnhldfn nfdh
+dhn dflhndéfnh kdfnh 
+dh ndfkh nfdlhn dflkhnndfhnldnf hdf
+gnfgdnlng dfg ,
+asdasdasggsjkgjfkdg
+gfdlhnd lndflhn ldfnhldfn nfdh
+dhn dflhndéfnh kdfnh 
+dh ndfkh nfdlhn dflkhnndfhnldnf hdf
+gnfgdnlng dfg 
+asdasdasggsjkgjfkdg
+gfdlhnd lndflhn ldfnhldfn nfdh
+dhn dflhndéfnh kdfnh 
+dh ndfkh nfdlhn dflkhnndfhnldnf hdf
+gnfgdnlng dfg 
+asdasdasggsjkgjfkdg
+gfdlhnd lndflhn ldfnhldfn nfdh
+dhn dflhndéfnh kdfnh 
+dh ndfkh nfdlhn dflkhnndfhnldnf hdf
+gnfgdnlng dfg 
+asdasdasggsjkgjfkdg
+gfdlhnd lndflhn ldfnhldfn nfdh
+dhn dflhndéfnh kdfnh 
+dh ndfkh nfdlhn dflkhnndfhnldnf hdf
+gnfgdnlng dfg 
+asdasdasggsjkgjfkdg
+gfdlhnd lndflhn ldfnhldfn nfdh
+dhn dflhndéfnh kdfnh 
+dh ndfkh nfdlhn dflkhnndfhnldnf hdf
+gnfgdnlng dfg 
+</pre>
+                </div>
+            </div>
+
+        </div> -->
+
+
+
+        <script src="" async defer></script>
+    </body>
+</html>
